f164cef71abfe0e99920e728c18d50417b681da5
Santiago Montero <smontero@seg-pichincha.ec>
2018-10-22 09:44:52 -0500

    module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,10 @@ import { AuthGuard } from './services/auth_guard.service';
 import { LoginComponent } from './sdp/login/login.component';
 import { DashboardComponent } from './sdp/dashboard/dashboard.component';
 import { EmpresasComponent } from './sdp/empresas/empresas.component';
-<<<<<<< HEAD
 import { UsuariosComponent } from './sdp/usuarios/usuarios.component';
-=======
 import { PerfilesComponent } from './sdp/perfiles/perfiles.component';
 import { OpcionesComponent } from './sdp/opciones/opciones.component';
 import { PermisosComponent } from './sdp/permisos/permisos.component';
->>>>>>> d37a61a53139695f01c73aed4eeb48abe3a57652
 
 export const routes: Routes = [
     {
@@ -26,15 +23,12 @@ export const routes: Routes = [
             { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
             { path: 'dashboard', component: DashboardComponent },
             { path: 'empresas', component: EmpresasComponent },
-<<<<<<< HEAD
             { path: 'usuarios', component: UsuariosComponent},
 
            
-=======
             { path: 'perfiles', component: PerfilesComponent },
             { path: 'opciones', component: OpcionesComponent },
             { path: 'permisos', component: PermisosComponent },
->>>>>>> d37a61a53139695f01c73aed4eeb48abe3a57652
 
 
 
