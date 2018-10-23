6804d224e814bcfd0f2b396edc08d09e1d016579
Dovannys Hernandez <dohernandez@seg-pichincha.ec>
2018-10-23 16:11:34 -0500

    Se elimina el envio de la contraseña dentro de la pantalla usuario. Se agrgan los campos con dropdown Perfil y Empresa.

--- src/app/sdp/usuarios/usuarios.component.ts
+++ src/app/sdp/usuarios/usuarios.component.ts
@@ -5,6 +5,7 @@ import { AppComponent } from 'src/app/app.component';
 import { URLSearchParams} from '@angular/http';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 import { Usuario } from 'src/app/domain/usuario';
+import { SelectItem } from 'primeng/components/common/selectitem';
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
@@ -24,6 +25,9 @@ export class UsuariosComponent implements OnInit {
 
   cols: any[];
   formulario: FormGroup;
+
+  perfiles: SelectItem[];
+  empresas: SelectItem[];
   
   constructor(private api: ApiRequestService,
     private formBuilder: FormBuilder,
@@ -35,12 +39,14 @@ export class UsuariosComponent implements OnInit {
       return this.formulario = this.formBuilder.group({
         usu_codigo_usuario: ['',[Validators.required]],
         usu_nombre_usuario: ['',[Validators.required]],
+        per_codigo_perfil: ['',[Validators.required]],
+        emp_id_empresa: ['',[Validators.required]],
         usu_mail: ['',[Validators.required]],
         usu_agencia: ['',[Validators.required]],
         usu_val_presupuesto: ['',],
         usu_nombre_asesor: ['',],
-        usu_contrasenia: ['',[Validators.required]],
-        usu_contrasenia_confirm: ['',[Validators.required]],
+        //usu_contrasenia: ['',[Validators.required]],
+        //usu_contrasenia_confirm: ['',[Validators.required]],
         usu_estado: ['A',],
         usu_usuario_crea: ['',],
       });  
@@ -57,13 +63,47 @@ export class UsuariosComponent implements OnInit {
       }
     );
 
+    this.api.get('/api/Perfiles/listar' , 'Usuarios').subscribe(
+      res => {
+        this.perfiles = [];
+        if (res != null) {
+          this.perfiles.push({ label: 'Elegir Perfil', value: null })
+          for (let i = 0; i < res.length; i++) {
+            this.perfiles.push({ label: res[i].per_descripcion, value: res[i].per_codigo_perfil });
+          }
+        }
+      });
+
+      this.api.get('/api/empresas/listar' , 'Usuarios').subscribe(
+        res => {
+          this.empresas = [];
+          if (res != null) {
+            this.empresas.push({ label: 'Elegir Empresa', value: null })
+            for (let i = 0; i < res.length; i++) {
+              this.empresas.push({ label: res[i].emp_razon_social, value: res[i].emp_id_empresa });
+            }
+          }
+        });
+
     this.cols = [
       { field: 'usu_codigo_usuario', header: 'Código' },
-      { field: 'usu_nombre_usuario', header: 'Nombre completo' },
+      { field: 'usu_nombre_usuario', header: 'Usuario' },
+      { field: 'usu_nombre_asesor', header: 'Nombre Completo'},
       { field: 'usu_mail', header: 'Correo electrónico' },
-      { field: 'usu_agencia', header: 'Agencia' },
-      { field: 'usu_usuario_crea', header: 'Usuario crea/modifica'}
+      //{ field: 'emp_id_empresa', header: 'Empresa' },
+      //{ field: 'usu_usuario_crea', header: 'Usuario crea/modifica'}
   ];
+  }
+
+  seleccionarPerfil(evento ) {
+
+    let idPerfil = evento.value;
+
+    this.formulario.patchValue({per_codigo_perfil: idPerfil});
+  }
+
+  seleccionarEmpresa(evento ) {
+    this.formulario.patchValue({emp_id_empresa: evento.value});
   }
 
   showDialogToAdd() {
@@ -75,11 +115,12 @@ export class UsuariosComponent implements OnInit {
     const envio = {
       usu_codigo_usuario: this.formulario.get('usu_codigo_usuario').value,
       usu_nombre_usuario: this.formulario.get('usu_nombre_usuario').value,
+      usu_nombre_asesor: this.formulario.get('usu_nombre_asesor').value,
+      per_codigo_perfil: this.formulario.get('per_codigo_perfil').value,
+      emp_id_empresa: this.formulario.get('emp_id_empresa').value,
       usu_mail: this.formulario.get('usu_mail').value,
       usu_agencia: this.formulario.get('usu_agencia').value,
       usu_val_presupuesto: this.formulario.get('usu_val_presupuesto').value,
-      usu_contrasenia: this.formulario.get('usu_contrasenia').value,
-      usu_contrasenia_confirm: this.formulario.get('usu_contrasenia_confirm').value,
       usu_estado: this.formulario.get('usu_estado').value,
       //usu_usuario_crea: this.formulario.get('usu_usuario_crea').value,
   };
@@ -115,11 +156,14 @@ export class UsuariosComponent implements OnInit {
     this.formulario = this.formBuilder.group({
       usu_codigo_usuario: [this.selectedUsuarios.usu_codigo_usuario, Validators.required],
       usu_nombre_usuario: [this.selectedUsuarios.usu_nombre_usuario, Validators.required],
-      usu_mail: [this.selectedUsuarios.usu_mail,],
+      usu_nombre_asesor: [this.selectedUsuarios.usu_nombre_asesor,],
+      per_codigo_perfil: [this.selectedUsuarios.per_codigo_perfil, Validators.required],
+      emp_id_empresa: [this.selectedUsuarios.per_codigo_perfil, Validators.required],
+      usu_mail: [this.selectedUsuarios.usu_mail, Validators.required],
       usu_agencia: [this.selectedUsuarios.usu_agencia, Validators.required],
       usu_val_presupuesto: [this.selectedUsuarios.usu_val_presupuesto,],
-      usu_contrasenia: [this.selectedUsuarios.usu_contrasenia,],
-      usu_contrasenia_confirm: [this.selectedUsuarios.usu_contrasenia_confirm,],
+      //usu_contrasenia: [this.selectedUsuarios.usu_contrasenia,],
+      //usu_contrasenia_confirm: [this.selectedUsuarios.usu_contrasenia_confirm,],
       usu_estado: ['A',],
       //usu_usuario_crea: [this.selectedUsuarios.usu_usuario_crea,],
     });
