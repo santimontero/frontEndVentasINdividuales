57a9f8cc5d7cad6e71eb6a6baf90a9b31125450c
Santiago Montero <smontero@seg-pichincha.ec>
2018-11-13 12:05:47 -0500

    template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,150 @@ import { Subject } from 'rxjs';
 export class AppComponent implements OnInit {
   title = 'frontEndVentasIndividuales';
   private items: any[];
+  menuMode = 'static';
+
+    topbarMenuActive: boolean;
+
+    overlayMenuActive: boolean;
+
+    staticMenuDesktopInactive: boolean;
+
+    staticMenuMobileActive: boolean;
+
+    layoutMenuScroller: HTMLDivElement;
+
+    lightMenu = true;
+
+    menuClick: boolean;
+
+    topbarItemClick: boolean;
+
+    activeTopbarItem: any;
+
+    resetMenu: boolean;
+
+    menuHoverActive: boolean;
+
+    rightPanelActive: boolean;
+
+    rightPanelClick: boolean;
+
+    onLayoutClick() {
+        if (!this.topbarItemClick) {
+            this.activeTopbarItem = null;
+            this.topbarMenuActive = false;
+        }
+
+        if (!this.rightPanelClick) {
+            this.rightPanelActive = false;
+        }
+
+        if (!this.menuClick) {
+            if (this.isHorizontal() || this.isSlim()) {
+                this.resetMenu = true;
+            }
+
+            if (this.overlayMenuActive || this.staticMenuMobileActive) {
+                this.hideOverlayMenu();
+            }
+
+            this.menuHoverActive = false;
+        }
+
+        this.topbarItemClick = false;
+        this.menuClick = false;
+        this.rightPanelClick = false;
+    }
+
+    onMenuButtonClick(event) {
+        this.menuClick = true;
+        this.topbarMenuActive = false;
+
+        if (this.isOverlay()) {
+            this.overlayMenuActive = !this.overlayMenuActive;
+        }
+        if (this.isDesktop()) {
+            this.staticMenuDesktopInactive = !this.staticMenuDesktopInactive;
+        } else {
+            this.staticMenuMobileActive = !this.staticMenuMobileActive;
+        }
+
+        event.preventDefault();
+    }
+
+    onMenuClick($event) {
+        this.menuClick = true;
+        this.resetMenu = false;
+    }
+
+    onTopbarMenuButtonClick(event) {
+        this.topbarItemClick = true;
+        this.topbarMenuActive = !this.topbarMenuActive;
+
+        this.hideOverlayMenu();
+
+        event.preventDefault();
+    }
+
+    onTopbarItemClick(event, item) {
+        this.topbarItemClick = true;
+
+        if (this.activeTopbarItem === item) {
+            this.activeTopbarItem = null;
+        } else {
+            this.activeTopbarItem = item;
+        }
+
+        event.preventDefault();
+    }
+
+    onTopbarSubItemClick(event) {
+        event.preventDefault();
+    }
+
+    onRightPanelButtonClick(event) {
+        this.rightPanelClick = true;
+        this.rightPanelActive = !this.rightPanelActive;
+        event.preventDefault();
+    }
+
+    onRightPanelClick() {
+        this.rightPanelClick = true;
+    }
+
+    isHorizontal() {
+        return this.menuMode === 'horizontal';
+    }
+
+    isSlim() {
+        return this.menuMode === 'slim';
+    }
+
+    isOverlay() {
+        return this.menuMode === 'overlay';
+    }
+
+    isStatic() {
+        return this.menuMode === 'static';
+    }
+
+    isMobile() {
+        return window.innerWidth < 1025;
+    }
+
+    isDesktop() {
+        return window.innerWidth > 1024;
+    }
+
+    isTablet() {
+        const width = window.innerWidth;
+        return width <= 1024 && width > 640;
+    }
+
+    hideOverlayMenu() {
+        this.overlayMenuActive = false;
+        this.staticMenuMobileActive = false;
+    }
   constructor(private router: Router, public renderer: Renderer, public mensaje: AppMessages, 
     private api: ApiRequestService, private userInfoService: UserInfoService,
     public Compiler: Compiler
---
+++ src/app/app.menu.component.ts
@@ -0,0 +1,211 @@
+import { Component, Input, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { MenuItem, ScrollPanel } from 'primeng/primeng';
+import { AppComponent } from './app.component';
+import { ApiRequestService } from './services/api-request.service';
+
+@Component({
+    selector: 'app-menu',
+    templateUrl: './app.menu.component.html'
+})
+export class AppMenuComponent implements OnInit, AfterViewInit {
+
+    @Input() reset: boolean;
+
+    model: any[];
+
+    @ViewChild('layoutMenuScroller') layoutMenuScrollerViewChild: ScrollPanel;
+
+    constructor(public app: AppComponent,private api: ApiRequestService) { }
+
+    ngOnInit() {
+       
+            this.api.get('/api/Permisos/listarUrl/' +this.api.getInfoUsuario().perfil, 'menu').subscribe(
+                codigosMenu => {
+                  this.model = [];
+                  for (let i = 0; i < codigosMenu.length; i++) {
+                    var menu ;
+                    console.log(codigosMenu[i])
+                 var c ={
+                  label: codigosMenu[i].opc_descripcion, routerLink: [codigosMenu[i].opc_url]
+                 }
+                 console.log(c)
+                      this.model.push(c);
+                    
+                  };
+                }
+              )
+         
+    }
+
+    ngAfterViewInit() {
+        setTimeout(() => { this.layoutMenuScrollerViewChild.moveBar(); }, 100);
+    }
+
+    changeTheme(theme: string, scheme: string) {
+        const layoutLink: HTMLLinkElement = <HTMLLinkElement>document.getElementById('layout-css');
+        layoutLink.href = 'assets/layout/css/layout-' + theme + '.css';
+
+        const themeLink: HTMLLinkElement = <HTMLLinkElement>document.getElementById('theme-css');
+        themeLink.href = 'assets/theme/' + theme + '/theme-' + scheme + '.css';
+    }
+
+    onMenuClick(event) {
+        if (!this.app.isHorizontal()) {
+            setTimeout(() => {
+                this.layoutMenuScrollerViewChild.moveBar();
+            }, 450);
+        }
+
+        this.app.onMenuClick(event);
+    }
+}
+
+@Component({
+    /* tslint:disable:component-selector */
+    selector: '[app-submenu]',
+    /* tslint:enable:component-selector */
+    template: `
+        <ng-template ngFor let-child let-i="index" [ngForOf]="(root ? item : item.items)">
+            <li [ngClass]="{'active-menuitem': isActive(i)}" [class]="child.badgeStyleClass" *ngIf="child.visible === false ? false : true">
+                <a [href]="child.url||'#'" (click)="itemClick($event,child,i)" (mouseenter)="onMouseEnter(i)"
+                   *ngIf="!child.routerLink" [ngClass]="child.styleClass"
+                   [attr.tabindex]="!visible ? '-1' : null" [attr.target]="child.target">
+                    <i [ngClass]="child.icon"></i>
+                    <span>{{child.label}}</span>
+                    <i class="fa fa-fw fa-angle-down layout-menuitem-toggler" *ngIf="child.items"></i>
+                    <span class="menuitem-badge" *ngIf="child.badge">{{child.badge}}</span>
+                </a>
+
+                <a (click)="itemClick($event,child,i)" (mouseenter)="onMouseEnter(i)" *ngIf="child.routerLink"
+                   [routerLink]="child.routerLink" routerLinkActive="active-menuitem-routerlink" [fragment]="child.fragment"
+                   [routerLinkActiveOptions]="{exact: true}" [attr.tabindex]="!visible ? '-1' : null" [attr.target]="child.target">
+                    <i [ngClass]="child.icon"></i>
+                    <span>{{child.label}}</span>
+                    <i class="fa fa-fw fa-angle-down layout-menuitem-toggler" *ngIf="child.items"></i>
+                    <span class="menuitem-badge" *ngIf="child.badge">{{child.badge}}</span>
+                </a>
+                <div class="layout-menu-tooltip">
+                    <div class="layout-menu-tooltip-arrow"></div>
+                    <div class="layout-menu-tooltip-text">{{child.label}}</div>
+                </div>
+                <ul app-submenu [item]="child" *ngIf="child.items" [visible]="isActive(i)" [reset]="reset" [parentActive]="isActive(i)"
+                    [@children]="(app.isSlim()||app.isHorizontal())&&root ? isActive(i) ?
+                    'visible' : 'hidden' : isActive(i) ? 'visibleAnimated' : 'hiddenAnimated'"></ul>
+            </li>
+        </ng-template>
+    `,
+    animations: [
+        trigger('children', [
+            state('hiddenAnimated', style({
+                height: '0px'
+            })),
+            state('visibleAnimated', style({
+                height: '*'
+            })),
+            state('visible', style({
+                height: '*',
+                'z-index': 100
+            })),
+            state('hidden', style({
+                height: '0px',
+                'z-index': '*'
+            })),
+            transition('visibleAnimated => hiddenAnimated', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)')),
+            transition('hiddenAnimated => visibleAnimated', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)'))
+        ])
+    ]
+})
+export class AppSubMenuComponent {
+
+    @Input() item: MenuItem;
+
+    @Input() root: boolean;
+
+    @Input() visible: boolean;
+
+    _parentActive: boolean;
+
+    _reset: boolean;
+
+    activeIndex: number;
+
+    constructor(public app: AppComponent, public appMenu: AppMenuComponent) { }
+
+    itemClick(event: Event, item: MenuItem, index: number) {
+        if (this.root) {
+            this.app.menuHoverActive = !this.app.menuHoverActive;
+        }
+        // avoid processing disabled items
+        if (item.disabled) {
+            event.preventDefault();
+            return true;
+        }
+
+        // activate current item and deactivate active sibling if any
+        this.activeIndex = (this.activeIndex === index) ? null : index;
+
+        // execute command
+        if (item.command) {
+            item.command({ originalEvent: event, item: item });
+        }
+
+        // prevent hash change
+        if (item.items || (!item.url && !item.routerLink)) {
+            setTimeout(() => {
+                this.appMenu.layoutMenuScrollerViewChild.moveBar();
+            }, 450);
+            event.preventDefault();
+        }
+
+        // hide menu
+        if (!item.items) {
+            if (this.app.isHorizontal() || this.app.isSlim()) {
+                this.app.resetMenu = true;
+            } else {
+                this.app.resetMenu = false;
+            }
+
+            this.app.overlayMenuActive = false;
+            this.app.staticMenuMobileActive = false;
+            this.app.menuHoverActive = !this.app.menuHoverActive;
+        }
+    }
+
+    onMouseEnter(index: number) {
+        if (this.root && this.app.menuHoverActive && (this.app.isHorizontal() || this.app.isSlim())
+            && !this.app.isMobile() && !this.app.isTablet()) {
+            this.activeIndex = index;
+        }
+    }
+
+    isActive(index: number): boolean {
+        return this.activeIndex === index;
+    }
+
+    @Input() get reset(): boolean {
+        return this._reset;
+    }
+
+    set reset(val: boolean) {
+        this._reset = val;
+
+        if (this._reset && (this.app.isHorizontal() || this.app.isSlim())) {
+            this.activeIndex = null;
+        }
+    }
+
+    @Input() get parentActive(): boolean {
+        return this._parentActive;
+    }
+
+    set parentActive(val: boolean) {
+        this._parentActive = val;
+
+        if (!this._parentActive) {
+            this.activeIndex = null;
+        }
+    }
+}
