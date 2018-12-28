3d9940c61c946588f5f4093b6a5555ca9c05afff
Dovannys Hernandez <dohernandez@seg-pichincha.ec>
2018-12-28 10:32:35 -0500

    Con los catálogos de la pantalla forma-pago.

--- src/app/sdp/forma-pago/forma-pago.component.ts
+++ src/app/sdp/forma-pago/forma-pago.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 import { SelectItem } from 'primeng/primeng';
 import { FormControl, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms'
 import { AppComponent } from 'src/app/app.component';
+import { ApiRequestService } from 'src/app/services/api-request.service';
 @Component({
   selector: 'app-forma-pago',
   templateUrl: './forma-pago.component.html',
@@ -18,58 +19,84 @@ export class FormaPagoComponent implements OnInit {
   mes: SelectItem[];
   es: any;
   formulario: FormGroup;
-  constructor( private formBuilder: FormBuilder,
-    public appComponent: AppComponent) { 
+  constructor(private formBuilder: FormBuilder,
+    public appComponent: AppComponent, private api: ApiRequestService) {
     this.es = {
       firstDayOfWeek: 1,
-      dayNames: [ "domingo","lunes","martes","miércoles","jueves","viernes","sábado" ],
-      dayNamesShort: [ "dom","lun","mar","mié","jue","vie","sáb" ],
-      dayNamesMin: [ "D","L","M","X","J","V","S" ],
-      monthNames: [ "enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre" ],
-      monthNamesShort: [ "ene","feb","mar","abr","may","jun","jul","ago","sep","oct","nov","dic" ],
+      dayNames: ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"],
+      dayNamesShort: ["dom", "lun", "mar", "mié", "jue", "vie", "sáb"],
+      dayNamesMin: ["D", "L", "M", "X", "J", "V", "S"],
+      monthNames: ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"],
+      monthNamesShort: ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"],
       today: 'Hoy',
       clear: 'Borrar'
-  }
+    }
 
   }
 
   ngOnInit() {
-this.nuevofurmulario();
+    this.nuevofurmulario();
     this.tipo_form_pago = [];
-
-    this.tipo_form_pago.push({ label: 'DEB.AUT.CORRIENTE', value: 0 });
-    this.tipo_form_pago.push({ label: 'DEB.AUT.AHORRO', value: 1 });
-    this.tipo_form_pago.push({ label: 'DEB.AUT.TARJETA', value: 2 });
+    this.api.get('api/catalogos/formaspago', 'forma_pago').subscribe(
+      formpag => {
+        for (let i = 0; i < formpag.length; i++) {
+          var p = { label: formpag[i].cat_descripcion, value: { id: (i + 1), name: formpag[i].cat_descripcion, code: formpag[i].cat_id_catalogo } };
+          this.tipo_form_pago.push(p);
+        }
+      }
+    )
 
     this.bancos = [];
-    this.bancos.push({ label: 'pichincha',value: 0 });
-    this.bancos.push({ label: 'guayaquil',value: 1 });
+    this.api.get('api/catalogos/bancos', 'forma_pago').subscribe(
+      bcos => {
+        for (let i = 0; i < bcos.length; i++) {
+          var p = { label: bcos[i].cat_descripcion, value: { id: (i + 1), name: bcos[i].cat_descripcion, code: bcos[i].cat_id_catalogo } };
+          this.bancos.push(p);
+        }
+      }
+    )
 
     this.tipo_tarjeta = [];
-    this.tipo_tarjeta.push({ label: 'visa', value:  1,  });
-    this.tipo_tarjeta.push({ label: 'american', value:  2, });
-   
+    this.api.get('api/catalogos/tarjetas', 'forma_pago').subscribe(
+      tarj => {
+        for (let i = 0; i < tarj.length; i++) {
+          var p = { label: tarj[i].cat_descripcion, value: { id: (i + 1), name: tarj[i].cat_descripcion, code: tarj[i].cat_id_catalogo } };
+          this.tipo_tarjeta.push(p);
+        }
+      }
+    )
+
     this.pais_emisor = [];
-    this.pais_emisor.push({ label: 'ecuador', value:  1});
-    this.pais_emisor.push({ label: 'peru', value:2 });
-  
-  
+    this.api.get('api/catalogos/paises', 'forma_pago').subscribe(
+      paisemi => {
+        for (let i = 0; i < paisemi.length; i++) {
+          var p = { label: paisemi[i].cat_descripcion, value: { id: (i + 1), name: paisemi[i].cat_descripcion, code: paisemi[i].cat_id_catalogo } };
+          this.pais_emisor.push(p);
+        }
+      }
+    )
+
     this.mes = [];
-    this.mes.push({ label: 'enero', value:  1});
-    this.mes.push({ label: 'febrero', value:2 });
-  
-  
+    this.api.get('api/catalogos/meses', 'forma_pago').subscribe(
+      meses => {
+        for (let i = 0; i < meses.length; i++) {
+          var p = { label: meses[i].cat_descripcion, value: { id: (i + 1), name: meses[i].cat_descripcion, code: meses[i].cat_id_catalogo } };
+          this.mes.push(p);
+        }
+      }
+    )
+
   }
   nuevofurmulario() {
     return this.formulario = this.formBuilder.group({
-      tipo_form_pago:  new FormControl('', ),
-      banco: new FormControl('',),
-      numero:  new FormControl('', Validators.required ),
-      tipo_tarjeta: new FormControl('',),
-      pais_emisor: new FormControl('',),
-      ano_caducidad: new FormControl('',),
-      mes_caducidad: new FormControl('',),
-      cvv: new FormControl('',),
+      tipo_form_pago: new FormControl(''),
+      banco: new FormControl(''),
+      numero: new FormControl('', Validators.required),
+      tipo_tarjeta: new FormControl(''),
+      pais_emisor: new FormControl(''),
+      ano_caducidad: new FormControl(''),
+      mes_caducidad: new FormControl(''),
+      cvv: new FormControl(''),
     });
 
 
@@ -83,21 +110,21 @@ this.nuevofurmulario();
 
     if (this.formulario.valid) {
       this.appComponent.loader = true; //activar cargando
-    this.enviarPadre.emit({ index: this.activeIndex + 1});
+      this.enviarPadre.emit({ index: this.activeIndex + 1 });
 
-    this.appComponent.loader = false; //desactivar cargando 
-  } else {
-    Object.keys(this.formulario.controls).forEach(field => { // {1}
-      const control = this.formulario.get(field);
-      control.markAsDirty({ onlySelf: true });            // {2}
-      control.markAsTouched({ onlySelf: true });       // {3}
-    });
-           
-  }
+      this.appComponent.loader = false; //desactivar cargando 
+    } else {
+      Object.keys(this.formulario.controls).forEach(field => { // {1}
+        const control = this.formulario.get(field);
+        control.markAsDirty({ onlySelf: true });            // {2}
+        control.markAsTouched({ onlySelf: true });       // {3}
+      });
+
+    }
   }
 
   anterior() {
-    this.enviarPadre.emit({ index: this.activeIndex - 1});
+    this.enviarPadre.emit({ index: this.activeIndex - 1 });
   }
 }
 
